b02ccc3929b112e4bdf9b2cc8b6276650e183065
Derick <derick290801@gmail.com>
2022-05-25 15:11:33 -0400

    Session 2 ready

---
+++ Kata_Temperature/Test_Project_2/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using Source_Project_2;
+
+namespace Test_Project_2
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
