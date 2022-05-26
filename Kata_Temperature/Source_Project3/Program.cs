b092c9b9d104574e5d55abdd80624483ad89c579
Derick <derick290801@gmail.com>
2022-05-26 12:06:40 -0400

    Session 3  ready

---
+++ Kata_Temperature/Source_Project3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Source_Project3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Example
+    {
+
+    }
+}
---
+++ Kata_Temperature/Test_Project3/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using Source_Project3;
+
+namespace Test_Project3
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
